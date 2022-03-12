9aa5970108d044cb494a3a508bb01d280903f9be
nikoofficial <100592102+nikoofficial@users.noreply.github.com>
2022-03-12 14:17:00 +0700

    third commit

---
+++ Karaktergame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karaktergame
+{
+    internal class Programgame
+    {
+        static void Main(string[] args)
+        {
+            public class PlayerSatate
+        {
+                public enum {Jongkok, Berdiri, Tengkurap, Terbang };
+                public enum { W, S, X};
+                
+                
+            }
+        }
+    }
+}
